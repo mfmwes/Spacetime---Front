edf6ed1cd91f4a84acf1d70f7e1fbbd6b1be026a
Wesley <wesmilitao@gmail.com>
2023-05-17 12:53:00 -0300

    page home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import { User } from 'lucide-react'
+import Image from 'next/image'
+import nlwLogo from '../assets/nlw-logo.svg'
 
 export default function Home() {
   return (
@@ -21,6 +23,29 @@ export default function Home() {
             Crie sua conta <span>e salve suas memórias!</span>
           </p>
         </a>
+        {/* Hero */}
+        <div className="space-y-5">
+          <Image src={nlwLogo} alt="logotipo da next level week" />
+          <div className="max-w-[420px] space-y-4">
+            <h1 className="text-[40px] font-bold leading-tight text-gray-50">
+              Sua Cápsula do Tempo
+            </h1>
+            <p>
+              Colecione momentos marcantes da sua jornada e compartilhe (se
+              quiser) com o mundo!
+            </p>
+          </div>
+          <a
+            className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600"
+            href=""
+          >
+            CADASTRAR LEMBRANÇA
+          </a>
+        </div>
+        <div className="text-sm leading-relaxed ">
+          {' '}
+          Feito na NLW da Rocketseat
+        </div>
       </div>
       {/* Right  */}
       <div className="flex flex-col bg-[url(../assets/stars.svg)] bg-cover p-16">
